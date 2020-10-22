f8f2a2b8c5899d90bd80c4234914e1d0a7f8e87e
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-10-22 17:20:19 +0300

    added info card

--- Spicy Space/Scripts/Roadmap/Info_Card.gd
+++ Spicy Space/Scripts/Roadmap/Info_Card.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+onready var popup = $Popup
+onready var title = $Popup/Title
+onready var difficulty = $Popup/Difficuty/difficulty_level
+
 func _ready():
+	show_card("", "")
+
+func show_card(game_mode, difficulty_level):
+	title.text = game_mode
+	difficulty.text = difficulty_level
 	yield(get_tree().create_timer(1), "timeout")
-	$Popup.show()
+	popup.show()
+
+func _on_Close_btn_pressed():
+	popup.hide()
