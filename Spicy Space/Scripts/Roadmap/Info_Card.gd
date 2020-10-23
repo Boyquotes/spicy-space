7f83df10dd02b68bbb18e1989eaabb0989d3161c
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-10-23 14:36:52 +0300

    info card works properly on roadmap scene now

--- Spicy Space/Scripts/Roadmap/Info_Card.gd
+++ Spicy Space/Scripts/Roadmap/Info_Card.gd
@@ -1,17 +1,25 @@
-extends Node2D
+extends CanvasLayer
 
 onready var popup = $Popup
 onready var title = $Popup/Title
 onready var difficulty = $Popup/Difficuty/difficulty_level
 
-func _ready():
-	show_card("", "")
-
 func show_card(game_mode, difficulty_level):
 	title.text = game_mode
 	difficulty.text = difficulty_level
-	yield(get_tree().create_timer(1), "timeout")
+	#set color to difficulty text
+	if difficulty_level == "easy":
+		difficulty.modulate = Color.green
+	elif difficulty_level == "normal":
+		difficulty.modulate = Color.yellow
+	elif difficulty_level == "hard":
+		difficulty.modulate = Color.red 
+	yield(get_tree().create_timer(.5), "timeout")
 	popup.show()
 
 func _on_Close_btn_pressed():
 	popup.hide()
+
+func _on_Go_btn_pressed():
+	popup.hide()
+	get_parent().go_to_road()
--- Spicy Space/Scripts/Roadmap/Road.gd
+++ Spicy Space/Scripts/Roadmap/Road.gd
@@ -3,8 +3,10 @@ extends Node2D
 export(bool) var mode_completed = false
 export(bool) var skipped = false
 export(String, "null", "start", "random", "meteor shower", "dog fight", "repairshop", "planet") var game_mode
+export(String, "null", "easy", "normal", "hard") var difficulty
 
 onready var button = $Button
+onready var info_card = $Info_Card
 
 func _ready():
 	if mode_completed:
@@ -21,10 +23,13 @@ func road(status):
 		skipped = true
 		button.disabled = true
 
-func _on_Button_pressed():
-	if !mode_completed:
-		mode_completed = true
+func go_to_road():
+		mode_completed = true	
 		button.modulate = Color.green
 		get_tree().get_root().get_node("Main_Scene").prepare_game_mode(game_mode)
+
+func _on_Button_pressed():
+	if !mode_completed:
+		info_card.show_card(game_mode, difficulty)
 	else:
 		print("this level already completed")
