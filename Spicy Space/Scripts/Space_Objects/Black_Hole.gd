b18d743127db87f93fb6f6375647e64b87302837
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-10-28 18:05:27 +0300

    rotate planets and black holes

--- Spicy Space/Scripts/Space_Objects/Black_Hole.gd
+++ Spicy Space/Scripts/Space_Objects/Black_Hole.gd
@@ -5,6 +5,13 @@ signal entered_to_hole
 var target_planet
 var roadmap
 
+func _physics_process(delta):
+	_rotate(delta)
+
+func _rotate(delta):
+	var rot_speed = rad2deg(0.01)
+	self.rotation = (rotation + rot_speed * 1 * delta)
+
 func _on_Black_Hole_area_entered(area):
 	if area.name == "SpaceShip":
 		GameLogic.choosen_planet = target_planet
--- Spicy Space/Scripts/Space_Objects/Planet.gd
+++ Spicy Space/Scripts/Space_Objects/Planet.gd
@@ -4,6 +4,13 @@ signal explore_planet(result)
 
 export(bool) var livable_control = false
 
+func _physics_process(delta):
+	_rotate(delta)
+
+func _rotate(delta):
+	var rot_speed = rad2deg(0.0005)
+	self.rotation = (rotation + rot_speed * -1 * delta)
+
 func _on_Planet_area_entered(area):
 	if area.is_in_group("player"):
 		emit_signal("explore_planet", livable_control)
