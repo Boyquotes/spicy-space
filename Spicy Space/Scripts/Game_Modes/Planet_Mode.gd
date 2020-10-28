8cd5a8caa8af3dae41e4cae184663c6c30d2682b
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-10-28 16:55:55 +0300

    result and planet huds added

--- Spicy Space/Scripts/Game_Modes/Planet_Mode.gd
+++ Spicy Space/Scripts/Game_Modes/Planet_Mode.gd
@@ -1,15 +1,27 @@
 extends "res://Scripts/Game_Modes/Mode.gd"
 
 onready var planet_pos = $planet_pos
+onready var planet_hud = $Planet_HUD
+onready var result_hud = $Result_HUD
+
+var planet
 
 func _ready():
 	_prepare_planet()
+	yield(get_tree().create_timer(6), "timeout")
+	planet_hud.visible = false
 
 func _prepare_planet():
-	var planet = GameLogic.choosen_planet
+	planet = GameLogic.choosen_planet
 	planet.position = planet_pos.position
+	planet.connect("explore_planet", self, "show_result")
 	add_child(planet)
-	if planet.livable_control:
+
+func show_result(livable):
+	result_hud.visible = true
+	if livable:
 		print("This planet is livable :D")
+		result_hud.prepare_result("livable")
 	else:
 		print("This planet isn't livable :(")
+		result_hud.prepare_result("not livable")
--- Spicy Space/Scripts/Space_Objects/Planet.gd
+++ Spicy Space/Scripts/Space_Objects/Planet.gd
@@ -1,3 +1,9 @@
 extends Area2D
 
+signal explore_planet(result)
+
 export(bool) var livable_control = false
+
+func _on_Planet_area_entered(area):
+	if area.is_in_group("player"):
+		emit_signal("explore_planet", livable_control)
